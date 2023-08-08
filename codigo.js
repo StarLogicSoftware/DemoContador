c67a9118c8e1825e4fa4946b3ad7bdcac1bd0d83
StarLogicSoftware <starlogicsoftware@gmail.com>
2023-08-08 20:57:35 -0300

    Actualizacion
    Se agrego boton de reset para que el contador vuelva a cero

--- codigo.js
+++ codigo.js
@@ -1,8 +1,15 @@
-var boton = document.getElementById('boton')
-var contador = document.getElementById('contador')
-var cuenta = 0
+const boton = document.getElementById('boton')
+const botonReset = document.getElementById('boton')
+const contador = document.getElementById('contador')
+
+let cuenta = 0
 
 boton.onclick = function () {
     cuenta += 1
+    contador.textContent = cuenta
+}
+
+botonReset.onclick = function () {
+    cuenta = 0
     contador.textContent = cuenta
 }
